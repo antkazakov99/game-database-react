267d76921e863069eb5a54eea7be11d807d3d5c3
Антон <ant.kazakov99@gmail.com>
2023-11-15 18:24:58 +0300

    Update sign up form

--- src/app/(public)/user/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <div>It's registration page</div>
-  )
-}
--- src/components/auth-modal.tsx
+++ src/components/auth-modal.tsx
@@ -1,25 +1,57 @@
-import {Button, Form, Modal, Stack} from "react-bootstrap";
+import {Button, Form, FormText, Modal, Stack} from "react-bootstrap";
+import {useState} from "react";
+import Link from "next/link";
 
 export default function AuthModal({show, onHide}: { show: boolean, onHide: () => void }) {
-    return (
-        <Modal show={show} onHide={onHide} size={"sm"} centered>
-            <Modal.Header closeButton>
-                <Modal.Title>Войти</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form className={'m-2'}>
-                    <Form.Group className={"my-4"}>
-                        <Form.Control type={"email"} placeholder={"Email"}></Form.Control>
-                    </Form.Group>
-                    <Form.Group className={"my-4"}>
-                        <Form.Control type={"password"} placeholder={"Пароль"}></Form.Control>
-                    </Form.Group>
-                    <Stack className="mx-auto">
-                        <Button className={"mb-2"}>Войти</Button>
-                        <Button variant="outline-secondary" href={"/user/signup"}>Создать аккаунт</Button>
-                    </Stack>
-                </Form>
-            </Modal.Body>
-        </Modal>
-    );
+
+    const [signUp, setSignUp] = useState(false);
+
+    const handleSignUp = () => setSignUp(true);
+    const handleSignIn = () => setSignUp(false);
+
+    if (signUp) {
+        return (
+            <Modal show={show} onHide={onHide} size={"sm"} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Регистрация</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form className={'m-2'}>
+                        <Form.Group className={"my-4"}>
+                            <Form.Control type={"email"} placeholder={"Email"}></Form.Control>
+                        </Form.Group>
+                        <Form.Group className={"my-4"}>
+                            <Form.Control type={"password"} placeholder={"Пароль"}></Form.Control>
+                        </Form.Group>
+                        <Stack className="mx-auto">
+                            <Button className={"mb-2"}>Зарегистрироваться</Button>
+                            <small>Есть аккаунт? <Link href={""} onClick={handleSignIn}>Войти</Link></small>
+                        </Stack>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        );
+    } else {
+        return (
+            <Modal show={show} onHide={onHide} size={"sm"} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Вход</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form className={'m-2'}>
+                        <Form.Group className={"my-4"}>
+                            <Form.Control type={"email"} placeholder={"Email"}></Form.Control>
+                        </Form.Group>
+                        <Form.Group className={"my-4"}>
+                            <Form.Control type={"password"} placeholder={"Пароль"}></Form.Control>
+                        </Form.Group>
+                        <Stack className="mx-auto">
+                            <Button className={"mb-2"}>Войти</Button>
+                            <small>Нет аккаунта? <Link href={""} onClick={handleSignUp}>Зарегистрироваться</Link></small>
+                        </Stack>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+        );
+    }
 }
