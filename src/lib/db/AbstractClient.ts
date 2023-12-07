034535427e861073ca69084830dfd76dd27debfb
Антон <ant.kazakov99@gmail.com>
2023-12-07 16:35:59 +0300

    Client classes refactoring

--- src/lib/db/AbstractClient.ts
+++ src/lib/db/AbstractClient.ts
@@ -1,10 +1,37 @@
 import {Pool} from "pg";
 
-export default abstract class AbstractClient
-{
-    protected clientPool: Pool;
+export default abstract class AbstractClient<Type> {
+    private clientPool: Pool;
 
     constructor(clientPool: Pool) {
         this.clientPool = clientPool
     }
+
+    protected async findOne(query: string, params: any[] = []): Promise<Type | null> {
+        const client = await this.clientPool.connect();
+        const result = await client.query(query, params);
+        client.release();
+
+        if (result.rowCount === null) {
+            return null;
+        }
+
+        return this.createObject(result.rows[0]);
+    }
+
+    protected async findMany(query: string, params: any[] = []): Promise<Type[]> {
+        const client = await this.clientPool.connect();
+        const result = await client.query(query, params);
+        client.release();
+
+        return result.rows.map((value) => this.createObject(value));
+    }
+
+    protected async exec(query: string, params: any[] = []): Promise<void> {
+        const client = await this.clientPool.connect();
+        await client.query(query, params);
+        client.release();
+    }
+
+    protected abstract createObject(fields: object): Type;
 }
--- src/lib/db/GameClient.ts
+++ src/lib/db/GameClient.ts
@@ -2,41 +2,9 @@ import {Pool} from "pg";
 import Game from "@/lib/entries/Game";
 import AbstractClient from "@/lib/db/AbstractClient";
 
-export default class GameClient extends AbstractClient {
+export default class GameClient extends AbstractClient<Game> {
     constructor(clientPool: Pool) {
         super(clientPool);
-    }
-
-    async add(game: Game): Promise<void> {
-        // const query = 'INSERT INTO games(name, release, description, url) VALUES($1, $2, $3, $4)';
-        //
-        // await this.client.connect();
-        //
-        // try {
-        //     await this.client.query('BEGIN');
-        //     const result = await this.client.query(query, [
-        //         game.name,
-        //         game.release,
-        //         game.description,
-        //         game.url
-        //     ]);
-        //     // todo Add other data addition
-        //     await this.client.query('COMMIT');
-        // } catch (e) {
-        //     await this.client.query('ROLLBACK');
-        //     throw e;
-        // } finally {
-        //     await this.client.end();
-        // }
-    }
-
-    delete(id: number): void {
-        // todo Add game delete
-    }
-
-    getByFilter(filter: AbstractFilter<Game>, {page, size}: { page: number, size: number }): Game[] {
-        // todo Add select by filter
-        return [];
     }
 
     async getAll({page, size}: { page: number | null, size: number | null } = {
@@ -47,36 +15,27 @@ export default class GameClient extends AbstractClient {
         const offset = size != null && page != null ? size * (page - 1) : 0;
         const query = `SELECT games.id, games.name, games.release, games.description, games.url FROM games LIMIT ${limit} OFFSET ${offset}`;
 
-        const client = await this.clientPool.connect();
-        const result = await client.query(query);
-        client.release();
-
-        return result.rows.map((value: {
-            id: string,
-            name: string,
-            release: string,
-            description: string,
-            url: string
-        }) => {
-            return new Game(parseInt(value['id']), value['name'], new Date(value['release']), value['description'], value['url']);
-        });
+        return this.findMany(query);
     }
 
     async getById(id: number): Promise<Game | null> {
         const query = 'SELECT games.id, games.name, games.release, games.description, games.url FROM games WHERE games.id = $1';
-        const client = await this.clientPool.connect();
-        const result = await client.query(query, [id]);
-        client.release();
-
-        if (!result.rowCount) {
-            return null;
-        }
-
-        const game: { id: string, name: string, release: string, description: string, url: string } = result.rows[0];
-        return new Game(parseInt(game.id), game.name, new Date(game.release), game.description, game.url);
+        return this.findOne(query);
     }
 
-    update(game: Game): void {
-        // todo Add game update
+    protected createObject(fields: {
+        id: number,
+        name: string,
+        release: string | null,
+        description: string,
+        url: string
+    }): Game {
+        return new Game(
+            fields.id,
+            fields.name,
+            fields.release !== null ? new Date(fields.release) : fields.release,
+            fields.description,
+            fields.url
+        );
     }
 }
